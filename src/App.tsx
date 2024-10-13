adf4ddfca4c080372c48e99a578c2703b2de9c61
eric-k-chu <kiyuen88ec@gmail.com>
2024-10-13 16:11:31 -0700

    wip colors

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const theme = createTheme({
 
 export default function App(): ReactElement {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} defaultColorScheme='dark'>
       <ClientLayout />
     </MantineProvider>
   )
