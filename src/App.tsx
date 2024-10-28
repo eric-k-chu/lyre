6f8afd441a3f14f0162d8da7020a68a33d1d5207
Eric Chu <Kiyuen88ec@gmail.com>
2024-10-27 21:05:20 -0700

    command

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { greet } from './api'
+import { Command } from '@tauri-apps/plugin-shell'
 
 function App() {
   const handleGreet = async (): Promise<void> => {
-    const msg = await greet({ name: 'World' })
-    if (msg.error !== undefined) {
-      alert(msg.error)
-      return
-    }
-    alert(msg.data)
+    // const msg = await greet({ name: 'World' })
+    // if (msg.error !== undefined) {
+    //   alert(msg.error)
+    //   return
+    // }
+    // alert(msg.data)
+
+    const output = await Command.create('echo', ['Hello', 'World']).execute()
+    alert(output.stdout)
+    console.log(output)
   }
 
   return (
